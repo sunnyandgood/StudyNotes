fb6eef1f965a618ce8a4a96ec6f5caaf69ecc9a2
一只有理想的菜鸟 <wrhsunny@163.com>
2018-09-10 21:21:42 +0800

    Update BackSquare.java

--- JAVA/练习/回型方阵/BackSquare.java
+++ JAVA/练习/回型方阵/BackSquare.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * 输入方阵阶数，输出回型方阵
  * eg:
@@ -15,39 +16,43 @@
  *
  */
 public class BackSquare {
-	public static void main(String[] args) {
-		BackSquare backSquare = new BackSquare();
-		int rank = 1;
-		String[][] square = backSquare.square(rank);
-		for(int i=0;i<rank*2;i++) {
-			for(int j=0;j<rank*2;j++) {
-				System.out.print(square[i][j]);
-			}
-			System.out.println();
-		}
-	}
-	
-	public String[][] square(int rank) {
-		int value = rank*2;
-		String[][] shape = new String[value][value];
-		for(int i=0;i<rank;i++) {
-			for(int j=value-1-i;j>=i;j--) {
-				for(int k=value-1-i;k>=i;k--) {
-					if(j==i) {
-						shape[j][k] = i+1+" ";
-					}
-					if(k==i) {
-						shape[j][k] = i+1+" ";
-					}
-					if(j==value-1-i) {
-						shape[j][k] = i+1+" ";
-					}
-					if(k==value-1-i) {
-						shape[j][k] = i+1+" ";
-					}
-				}
-			}
-		}
-		return shape;		
-	}
+        public static void main(String[] args) {
+                BackSquare backSquare = new BackSquare();
+                int rank;
+                Scanner input  = new Scanner(System.in);
+                rank = input.nextInt();
+                String[][] square = backSquare.square(rank);
+                for(int i=0;i<rank;i++) {
+                        for(int j=0;j<rank;j++) {
+                                System.out.print(square[i][j]);
+                        }
+                        System.out.println();
+                }
+        }
+        
+        public String[][] square(int rank) {
+                String[][] shape = new String[rank][rank];
+                for(int i=0;i<rank/2;i++) {
+                        for(int j=rank-1-i;j>=i;j--) {
+                                for(int k=rank-1-i;k>=i;k--) {
+                                        if(j==i) {
+                                                shape[j][k] = i+1+" ";
+                                        }
+                                        if(k==i) {
+                                                shape[j][k] = i+1+" ";
+                                        }
+                                        if(j==rank-1-i) {
+                                                shape[j][k] = i+1+" ";
+                                        }
+                                        if(k==rank-1-i) {
+                                                shape[j][k] = i+1+" ";
+                                        }
+                                        if(rank%2!=0) {
+                                                shape[(rank-1)/2][(rank-1)/2] = rank/2+1+" ";
+                                        }
+                                }
+                        }
+                }
+                return shape;           
+        }
 }
